c76edd05c07c8fd66d7979499f55cc23f20d9134
YORESTARLING MEJIA BERAS <ym2018-0345@uce.edu.do>
2023-02-06 22:54:42 -0400

    Adding jwtTokenGenerator n' is wrking

--- ApiDDD.Application/Services/Auth/AuthService.cs
+++ ApiDDD.Application/Services/Auth/AuthService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ApiDDD.Application.Common.Interfaces.Auth;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,18 @@ using System.Threading.Tasks;
 
 namespace ApiDDD.Application.Services.Auth
 {
+     
     public class AuthService : IAuthService
     {
+
+        private readonly IJwtTokenGenerator _jtwTokenGenerator;
+
+        public AuthService(IJwtTokenGenerator jtwTokenGenerator)
+        {
+            _jtwTokenGenerator = jtwTokenGenerator;
+        }
+
+        
         public AuthResult Login(string email, string password)
         {
             return new AuthResult(
@@ -20,12 +31,23 @@ namespace ApiDDD.Application.Services.Auth
 
         public AuthResult Register(string firstName, string lastName, string email, string password)
         {
+
+            //Check if user already exists
+
+            //Create user (generate uniqueId)
+
+            //Create JWT Token
+            Guid userId = Guid.NewGuid();
+            var token = _jtwTokenGenerator.GenerateToken(userId,firstName, lastName);
+
+
+
             return new AuthResult(
                 Guid.NewGuid(),
                 firstName,
                 lastName,
                 email,
-                "token");
+                token);
         }
     }
 }
--- ApiDDD.Infrastructure/DependencyInjection.cs
+++ ApiDDD.Infrastructure/DependencyInjection.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using ApiDDD.Application.Common.Interfaces.Auth;
 using ApiDDD.Application.Services.Auth;
+using ApiDDD.Infrastructure.Auth;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ApiDDD.Infrastructure
@@ -13,7 +15,7 @@ namespace ApiDDD.Infrastructure
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection service)
         {
-            
+            service.AddSingleton<IJwtTokenGenerator,JwtTokenGenerator>();
             return service;
         }
 
